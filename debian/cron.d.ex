74c32cdc173d4dd5a0d54bbf9d306f4719bdc77c
Gerrit Pape <pape@smarden.org>
2000-12-18 15:08:35 +0000

    cleanup.

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the qmail-run package
-#
-0 4	* * *	root	qmail-run_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(qmail-run):needs=X11|text|vc|wm section=Apps/see-menu-manual\
-  title="qmail-run" command="/usr/bin/qmail-run"
